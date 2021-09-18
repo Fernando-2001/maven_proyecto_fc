d594884fb75d70f69ba7a5615b5a76e25a9a551d
Fernando Chuquimarca <jf7_chuquimarca@hotmail.com>
2021-09-17 20:10:25 -0500

    List.

---
+++ src/main/java1/ejercicios/coleccion/list/Main.java
@@ -0,0 +1,23 @@
+package ejercicios.coleccion.list;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Integer numero1 = 1;
+		Integer numero2 = 4;
+		Integer numero3 = 8;
+		Integer numero4 = 20;
+		
+		// TODO Auto-generated method stub
+		ArrayList<Integer> listaNueva= new ArrayList<Integer>();
+		listaNueva.add(numero1);
+		listaNueva.add(numero2);
+		listaNueva.add(numero3);
+		listaNueva.add(numero4);
+		
+	}
+
+}
