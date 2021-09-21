6492f2ef09f3ef91854ffacfd60ad9342fb80943
Fernando Chuquimarca <jf7_chuquimarca@hotmail.com>
2021-09-20 20:05:01 -0500

    Taller 22.

---
+++ src/main/java1/ejercicios/fechas/Instructor.java
@@ -0,0 +1,5 @@
+package ejercicios.fechas;
+
+public class Instructor {
+
+}
--- src/main/java1/ejercicios/fechas/Main.java
+++ src/main/java1/ejercicios/fechas/Main.java
@@ -1,6 +1,7 @@
 package ejercicios.fechas;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Main {
@@ -56,7 +57,12 @@ public class Main {
 		System.out.println("Nombre: "+curso3.getNombre());
 		System.out.println("F I: "+curso3.getFechaInicio());
 		System.out.println("F F: "+curso3.getFechaFin());
+		System.out.println("F F Formateada: "+curso3.getFechaFin().format(DateTimeFormatter.ofPattern("MM_dd-yyyy")));
 		
+		System.out.println("Que dia es hoy: ");
+		LocalDate diaHoy3 = LocalDate.now();
+		System.out.println(diaHoy3.format(DateTimeFormatter.ofPattern("MM-dd-yyyy")));
+		System.out.println(diaHoy3.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
 		}
 
 }
